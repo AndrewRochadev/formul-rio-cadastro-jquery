cf4afa7ad41081b4c8cbb9bfc191c7cd4c5390d1
Andrew <andrew.pablo66@gmail.com>
2024-02-22 15:34:59 -0300

    initial commit fomulário

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            telefone: {
+                required: true,
+            },
+            cpf: {
+                required: true,
+            },
+            email: {
+                required: true,
+            },
+            enderecoCompleto: {
+                required: true,
+            },
+            cep: {
+                required: true,
+            },
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome completo'
+        },
+        messages: {
+            telefone: 'Por favor, insira o seu telefone'
+        },
+        messages: {
+            cpf: 'Por favor, insira o seu CPF'
+        },
+        messages: {
+            email: 'Por favor, insira o seu email'
+        },
+        messages: {
+            enderecoCompleto: 'Por favor, insira o seu endereço completo'
+        },
+        messages: {
+            cep: 'Por favor, insira o seu CEP'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids()
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos Incorretos`)
+            }
+        }
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+        })
+    })
+})
