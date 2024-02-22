1971ecfeacd6324af788fc1d815d1fd9a8c906b1
Andrew <andrew.pablo66@gmail.com>
2024-02-22 16:00:32 -0300

    initial commit form

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
             }
         }
         form.addEventListener('submit', function(e) {
-            e.preventDefault();
+        e.preventDefault();
         })
     })
 })
